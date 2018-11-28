cc5c52b34982636f6e1ac5ff87b6ae0ba11a8939
Phu Vo <vongocphu04@gmail.com>
2018-11-28 11:25:57 +0700

    v2

--- 1298-content.php
+++ 1298-content.php
@@ -13,24 +13,18 @@ $url_path = str_replace('\\', '/', $url_path);
             <div class="col-md-9">
                 <div class="col-md-4">
                     <div class="form-group">
-                        <div class="custom-select" style="width:100%;">
-                            <label for="">Main Location</label><br>
-                            <select>
-                                <option value="0">Select car:</option>
-                                <option value="1">Audi</option>
-                                <option value="2">BMW</option>
-                                <option value="3">Citroen</option>
-                                <option value="4">Ford</option>
-                                <option value="5">Honda</option>
-                                <option value="6">Jaguar</option>
-                                <option value="7">Land Rover</option>
-                                <option value="8">Mercedes</option>
-                                <option value="9">Mini</option>
-                                <option value="10">Nissan</option>
-                                <option value="11">Toyota</option>
-                                <option value="12">Volvo</option>
-                            </select>
-                        </div>
+
+                        <label for="">Main Location</label><br>
+                        <select class="select-hidden" name="" id="">
+                            <option value="">Any</option>
+                            <option value="">Any</option>
+                            <option value="">Any</option>
+                        </select>
+
+                    </div>
+                    <div class="select-custom">
+                        <div class="selected-item">aaaa</div>
+                        <div class="select-option">aaaa</div>
                     </div>
                 </div>
                 <div class="col-md-4">
@@ -38,7 +32,7 @@ $url_path = str_replace('\\', '/', $url_path);
                     <div class="form-group">
 
                         <label for="">Main Location</label><br>
-                        <select class="" name="" id="">
+                        <select class="select-hidden" name="" id="">
                             <option value="">Any</option>
                             <option value="">Any</option>
                             <option value="">Any</option>
@@ -51,7 +45,7 @@ $url_path = str_replace('\\', '/', $url_path);
                     <div class="form-group">
 
                         <label for="">Main Location</label>
-                        <select class="" name="" id="">
+                        <select class="select-hidden" name="" id="">
                             <option value="">Any</option>
                             <option value="">Any</option>
                             <option value="">Any</option>
