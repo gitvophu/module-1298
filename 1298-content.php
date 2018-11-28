e084bdbced5b26f24d086c2d068acdd330904ae8
Phu Vo <vongocphu04@gmail.com>
2018-11-28 23:33:53 +0700

    truyen du lieu nguoc lai tu select-custom vao select

--- 1298-content.php
+++ 1298-content.php
@@ -10,25 +10,21 @@ $url_path = str_replace('\\', '/', $url_path);
 <div class="type-1298">
 
     <div class="container">
-        <form method="get" action="#">
+        <form id="search-form" method="get" action="#">
             <div class="col-md-9">
                 <div class="col-md-4 select-box">
                     <div class="form-group" >
-
-
                         <select class="select-hidden" name="province" id="select-1">
 
                             <option value="1">HCM</option>
                             <option value="2">HN</option>
                             <option value="3">Da Nang</option>
                         </select>
-
                     </div>
                     <div class="select-custom" id="select-custom-1">
                         <div class="label" >Thanh Pho</div>
                         <div class="selected-item">selected</div>
                         <div class="select-options">
-
                             <div class="option-item">option 1</div>
                             <div class="option-item">option 2</div>
                             <div class="option-item">option 3</div>
@@ -40,9 +36,9 @@ $url_path = str_replace('\\', '/', $url_path);
                     <div class="form-group" >
 
                         <select class="select-hidden" name="district" id="select-2">
-                            <option value="">Quan 1</option>
-                            <option value="">Quan 2</option>
-                            <option value="">Quan 3</option>
+                            <option value="1">Quan 1</option>
+                            <option value="2">Quan 2</option>
+                            <option value="3">Quan 3</option>
                         </select>
 
                     </div>
@@ -61,9 +57,9 @@ $url_path = str_replace('\\', '/', $url_path);
                     <div class="form-group" >
 
                         <select class="select-hidden" name="ward" id="select-3">
-                            <option value="">Tan thoi hiep</option>
-                            <option value="">Trung chanh</option>
-                            <option value="">Hiep thanh</option>
+                            <option value="1">Tan thoi hiep</option>
+                            <option value="2">Trung chanh</option>
+                            <option value="3">Hiep thanh</option>
                         </select>
 
                     </div>
@@ -78,6 +74,27 @@ $url_path = str_replace('\\', '/', $url_path);
 
                     </div>
                 </div>
+               <!-- <div class="col-md-3 select-box" id="select-box-3">
+                    <div class="form-group" >
+
+                        <select class="select-hidden" name="ward" id="select-3">
+                            <option value="1">Tan thoi hiep</option>
+                            <option value="2">Trung chanh</option>
+                            <option value="3">Hiep thanh</option>
+                        </select>
+
+                    </div>
+                    <div class="select-custom" id="select-custom-3">
+                        <div class="label" >Phuong</div>
+                        <div class="selected-item">selected</div>
+                        <div class="select-options">
+                            <div class="option-item">option 1</div>
+                            <div class="option-item">option 2</div>
+                            <div class="option-item">option 3</div>
+                        </div>
+
+                    </div>
+                </div>-->
             </div>
 
             <div class="col-md-3">
--- js/1298.js
+++ js/1298.js
@@ -1,6 +1,8 @@
 $(document).ready(function () {
     getAndSetHeightSelecBoxToButton();
     loadDataSelect1();
+    loadDataSelect2();
+    loadDataSelect3();
 
     $('.selected-item').click(function (event) {
         resetSelectBoxes();
@@ -13,8 +15,20 @@ $(document).ready(function () {
     });
     $('.option-item').click(function () {
         $('.select-options').toggle(0);
-
-
+        var string = $(this).text();
+        var text = string.substr(0, string.length - 1);
+        var value = string.substr(string.length - 1, 1);
+        $(this).parent().prev('.selected-item').text(text);
+        var current_select_custom_id = $(this).parent().parent().attr('id');
+        console.log('current id: ' + current_select_custom_id);
+        switch (current_select_custom_id) {
+            case 'select-custom-1':
+                $(`#select-1 option[value=${value}]`).prop('selected', true);
+            case 'select-custom-2':
+                $(`#select-2 option[value=${value}]`).prop('selected', true);
+            case 'select-custom-3':
+                $(`#select-3 option[value=${value}]`).prop('selected',true);
+        }
     });
     $(document).click(function () {
         $('.select-options').hide();
@@ -22,6 +36,10 @@ $(document).ready(function () {
         $('.selected-item').css('color', '#000')
         $('.label').css('color', '#000')
     });
+    $('.search-btn').click(function () {
+        $('#search-form').submit();
+    });
+
 
 });
 
@@ -52,7 +70,46 @@ function loadDataSelect1() {
 
     $('#select-custom-1').find('.select-options').empty();
     data.map((item, index) => {
-
-        $('#select-custom-1').find('.select-options').append(`<div class="option-item">${item.text}<span>${item.value}</span></div>`);
+        $('#select-custom-1').find('.select-options')
+            .append(`<div class="option-item">${item.text}<span>${item.value}</span></div>`);
+    })
+    ;
+}
+function loadDataSelect2() {
+    var data = [];
+    var options = $('#select-2').children().each(function (index) {
+        var value = $(this).attr('value');
+        var text = $(this).text();
+        data.push(
+            {
+                'value': value,
+                'text': text
+            });
     });
+
+    $('#select-custom-2').find('.select-options').empty();
+    data.map((item, index) => {
+        $('#select-custom-2').find('.select-options')
+            .append(`<div class="option-item">${item.text}<span>${item.value}</span></div>`);
+    })
+    ;
+}
+function loadDataSelect3() {
+    var data = [];
+    var options = $('#select-3').children().each(function (index) {
+        var value = $(this).attr('value');
+        var text = $(this).text();
+        data.push(
+            {
+                'value': value,
+                'text': text
+            });
+    });
+
+    $('#select-custom-3').find('.select-options').empty();
+    data.map((item, index) => {
+        $('#select-custom-3').find('.select-options')
+            .append(`<div class="option-item">${item.text}<span>${item.value}</span></div>`);
+    })
+    ;
 }
